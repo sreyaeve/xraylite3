1aa9826efc7fb6163567d941b2216ca30053abca
sreyaeve <121951124+sreyaeve@users.noreply.github.com>
2023-04-24 22:02:16 +0700

    Update gotop.sh

--- gotop.sh
+++ gotop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Information
+mkdir /usr/local/etc/xray
 curl -s ipinfo.io/city >> /usr/local/etc/xray/city
 curl -s ipinfo.io/org | cut -d " " -f 2-10 >> /usr/local/etc/xray/org
 curl -s ipinfo.io/timezone >> /usr/local/etc/xray/timezone
