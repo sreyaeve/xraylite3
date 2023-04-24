4f55ced835bd358396c7e708777974c806397bfb
sreyaeve <121951124+sreyaeve@users.noreply.github.com>
2023-04-24 22:00:43 +0700

    Update gotop.sh

--- gotop.sh
+++ gotop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#Install Gotop
-snap install gotop-cjbassi
-
+#Information
+curl -s ipinfo.io/city >> /usr/local/etc/xray/city
+curl -s ipinfo.io/org | cut -d " " -f 2-10 >> /usr/local/etc/xray/org
+curl -s ipinfo.io/timezone >> /usr/local/etc/xray/timezone
