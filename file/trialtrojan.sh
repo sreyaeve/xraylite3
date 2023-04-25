82924dc353eddac3da510bbbbca05cbeca17f329
sreyaeve <121951124+sreyaeve@users.noreply.github.com>
2023-04-25 20:16:05 +0700

    Update trialtrojan.sh

--- file/trialtrojan.sh
+++ file/trialtrojan.sh
@@ -14,7 +14,7 @@ ISP=$(cat /usr/local/etc/xray/org)
 CITY=$(cat /usr/local/etc/xray/city)
 tls="$(cat ~/log-install.txt | grep -w "Trojan WS TLS" | cut -d: -f2|sed 's/ //g')"
 ntls="$(cat ~/log-install.txt | grep -w "Trojan WS none TLS" | cut -d: -f2|sed 's/ //g')"
-user=Trojan-`</dev/urandom tr -dc A-Z0-9 | head -c4`
+user=Trojan-`</dev/urandom tr -dc 0-9 | head -c3`
 uuid=$(cat /proc/sys/kernel/random/uuid)
 masaaktif=1
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
@@ -28,28 +28,28 @@ trojanlink1="trojan://${uuid}@${domain}:${tls}?mode=gun&security=tls&type=grpc&s
 trojanlink="trojan://${uuid}@isi_bug_disini:${tls}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
 trojanlink2="trojan://${uuid}@isi_bug_disini:${ntls}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws#${user}"
 clear
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "         Trial Trojan               "
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Remarks        : ${user}"
-echo -e "Host/IP        : ${domain}"
-echo -e "ISP            : ${ISP}" 
-echo -e "Region         : ${CITY}" 
-echo -e "Port TLS       : 443, 8443, 2053, 2083, 2087, 2096"
-echo -e "Port none TLS  : 80, 2082, 8880, 8080, 2095, 2086, 2052"
-echo -e "Port gRPC      : ${tls}"
-echo -e "Key            : ${uuid}"
-echo -e "Path           : /trojan-ws"
-echo -e "Service Name   : trojan-grpc"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link TLS       : ${trojanlink}"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link none TLS  : ${trojanlink2}"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link gRPC      : ${trojanlink1}"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Expired On     : $exp"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "         Trial Trojan               " | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Remarks        : ${user}" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Host/IP        : ${domain}" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "ISP            : ${ISP}" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Region         : ${CITY}" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Port TLS       : 443, 8443, 2053, 2083, 2087, 2096" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Port none TLS  : 80, 2082, 8880, 8080, 2095, 2086, 2052" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Port gRPC      : ${tls}" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Key            : ${uuid}" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Path           : /trojan-ws" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Service Name   : trojan-grpc" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Link TLS       : ${trojanlink}" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Link none TLS  : ${trojanlink2}" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Link gRPC      : ${trojanlink1}" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "Expired On     : $exp" | tee -a /etc/xraylog/log-trojan-$user.txt
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/xraylog/log-trojan-$user.txt
 echo ""
 read -n 1 -s -r -p "Press any key to back"
 
