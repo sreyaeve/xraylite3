295742885389f6b8ff4cb17fc8741bf770c14600
sreyaeve <121951124+sreyaeve@users.noreply.github.com>
2023-04-29 10:22:50 +0700

    Delete gotop.sh

--- gotop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Information
-mkdir /usr/local/etc/xray
-curl -s ipinfo.io/city >> /usr/local/etc/xray/city
-curl -s ipinfo.io/org | cut -d " " -f 2-10 >> /usr/local/etc/xray/org
-curl -s ipinfo.io/timezone >> /usr/local/etc/xray/timezone
